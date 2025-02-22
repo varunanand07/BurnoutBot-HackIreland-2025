15f8c78c58a0abe7fb6320b7ee94a875f0a95305
Jake Farrell <jake_farrell@outlook.com>
2025-02-22 22:31:22 +0000

    fix merge conflicts

--- src/server.js
+++ src/server.js
@@ -1,19 +1,9 @@
-<<<<<<< HEAD
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "../src/routes/authRoutes.js";
 import calendarRoutes from "../src/routes/calendarRoutes.js";
 
 dotenv.config();
-=======
-
-import express from 'express';
-import dotenv from 'dotenv';
-
-
-dotenv.config();  
-
->>>>>>> main
 
 const app = express();
 app.use(express.json());
@@ -23,18 +13,4 @@ app.use("/auth", authRoutes);
 app.use("/calendar", calendarRoutes);
 
 const PORT = process.env.PORT || 4000;
-<<<<<<< HEAD
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
-=======
-
-
-const server = express();
-
-
-console.log(`Using port: ${PORT}`);
-
-
-server.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
->>>>>>> main
