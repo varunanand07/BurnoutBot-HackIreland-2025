d265e1de769b5f09722d4c1020356d640628b6fe
Adrija <adrija.bhowmick21@gmail.com>
2025-02-22 15:43:51 +0000

    Initital setup

---
+++ src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import dotenv from "dotenv";
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+const server=express();
+console.log(PORT);
+
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
