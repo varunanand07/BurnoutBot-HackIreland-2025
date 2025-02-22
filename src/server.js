98ccbe5a6990a59be6449f47fdc30127c1a53088
varunanand07 <anandvarun2003@gmail.com>
2025-02-22 16:47:16 +0000

    Set up Slack Bot and Environment Variables

--- src/server.js
+++ src/server.js
@@ -1,13 +1,20 @@
+
 import express from 'express';
-import dotenv from "dotenv";
-dotenv.config();
+import dotenv from 'dotenv';
+
+
+dotenv.config();  
+
 
 const PORT = process.env.PORT || 4000;
 
-const server=express();
-console.log(PORT);
+
+const server = express();
+
+
+console.log(`Using port: ${PORT}`);
 
 
 server.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
