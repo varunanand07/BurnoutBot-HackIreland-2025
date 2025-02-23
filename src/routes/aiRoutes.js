eb93345f8444a9a170a2970bafddf02e046b5b8a
Jake Farrell <jake_farrell@outlook.com>
2025-02-23 00:43:01 +0000

    add ai endpoint for getting priorities

--- src/routes/aiRoutes.js
+++ src/routes/aiRoutes.js
@@ -1,24 +1,11 @@
-import Groq from "groq-sdk";
-const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+import express from "express";
+import { getSortPriority} from "../controllers/aiController.js";
 
-router.get("/chat-completion", async (req, res) => {
-  try {
-    const chatCompletion = await groq.chat.completions.create({
-      messages: [
-        {
-          role: "user",
-          content: "Explain the importance of fast language models",
-        },
-      ],
-      model: "llama-3.3-70b-versatile",
-    });
+const router = express.Router();
 
-    res.json({ response: chatCompletion.choices[0]?.message?.content || "" });
-  } catch (error) {
-    console.error("Error fetching chat completion:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
-});
+router.post("/sort-priority", getSortPriority);
 
-module.exports = router;
 
+
+
+export default router;
